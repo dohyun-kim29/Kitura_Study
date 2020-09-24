e2747a05c348f79e275b8f7363736f1a9c2fe871
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-24 14:07:18 +0900

    [Add] Client App

---
+++ ClientAPP/ClientAPP/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ClientAPP
+//
+//  Created by DohyunKim on 2020/09/24.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func request(_ sender: Any) {
+        
+        
+    }
+    
+}
+

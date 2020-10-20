dd7f5aafbec17b46b5a3736d1a1778234adadec8
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-20 14:41:26 +0900

    [Add] get method (file)

--- TestServer/Sources/TestServer/main.swift
+++ TestServer/Sources/TestServer/main.swift
@@ -1,15 +1,26 @@
 import Kitura
 import HeliumLogger
 import LoggerAPI
+import FileKit
+import Foundation
 
 HeliumLogger.use()
 
 let router = Router()
 
+let fileUrl = URL(fileURLWithPath: #file)
+
 router.get("/") {
     
     request, response, next in
     response.send(["hello":"Hello, Kitura!"])
+    next()
+}
+
+router.get("/file/") {
+    
+    request, response, next in
+    response.send(["file" : "\(fileUrl)"])
     next()
 }
 
